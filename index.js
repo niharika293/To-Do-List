22606e9304703b48d935701defca1a1bcfe6b1f6
Niharika Gurnani <niharikagurnani13@gmail.com>
2021-01-21 21:25:33 +0530

    Add new directories for extra features and scalable code

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// Acquiring Express
+// Step-1 : Acquiring Express
 const express = require('express');
 const app = express();//Creates an Express application
 const port = 8000;
-//Testing the server
+//Step-2 : Testing the server
 app.listen(port,function(err){
     if(err){
         console.log(`Error :${err} in running the server`);
@@ -10,3 +10,10 @@ app.listen(port,function(err){
     }
     console.log(`Server is up and running on the port: ${port}`);
 });
+// //Getting response from server
+// app.get('/',function(req,res){
+//     //return res.render('home');
+//     return res.end('<h1>Welcome to my to-do app </h1>');
+// });
+// Step-3 : Set-up Routes 
+
